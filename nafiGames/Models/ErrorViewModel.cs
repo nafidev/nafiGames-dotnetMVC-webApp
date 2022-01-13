8d82cfe402f5e1a5765b609feb4a9dfecb7b0abb
nafim <nafim@LAPTOP-UGMMA61J>
2022-01-13 16:07:59 +0100

    Ajoutez des fichiers projet.

---
+++ nafiGames/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace nafiGames.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
